7bbf3d3bb6cc9e780d415c9cd3cdadad77b99ffe
Moyses Azevedo <m.mirazev@gmail.com>
2022-08-08 15:43:46 -0300

    :white_check_mark: test: adicionando teste

--- IntegrationTests/IntegrationTests.swift
+++ IntegrationTests/IntegrationTests.swift
@@ -24,13 +24,11 @@ class IntegrationTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
+        let i = 0
+
+        XCTAssert(i == 0, "I is supposed to be 0")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
